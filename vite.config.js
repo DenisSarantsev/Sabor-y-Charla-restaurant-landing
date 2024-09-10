e7b4584fe7e518edd3458cdc7c0cbf834ef348ce
Denis <sarantsev.denis.94@gmail.com>
2024-09-10 23:53:01 +0300

    Добавлен суффикс dist в базовый путь

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
 // vite.config.js
 export default {
-  base: '/Sabor-y-Charla-restaurant-landing/', // Указываем путь к репозиторию
-  build: {
-    outDir: 'dist', // В папку dist будут выводиться результаты сборки
-  },
+  base: '/Sabor-y-Charla-restaurant-landing/dist/', // Указываем путь к репозиторию
 };
