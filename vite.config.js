5e9036f3ca55dc93243db238aca9527e6f21070f
Denis <sarantsev.denis.94@gmail.com>
2024-09-10 23:50:44 +0300

    Обновлены пути стилей

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+// vite.config.js
 export default {
-  base: '/Sabor-y-Charla-restaurant-landing/', // Укажи путь к репозиторию
+  base: '/Sabor-y-Charla-restaurant-landing/', // Указываем путь к репозиторию
+  build: {
+    outDir: 'dist', // В папку dist будут выводиться результаты сборки
+  },
 };
