ebb69aa25edbf64c4b588848e8e983b61a5bbabc
Denis <sarantsev.denis.94@gmail.com>
2024-09-30 08:24:06 +0300

    ЗАвершена верстка и адаптация первого блока

--- assets/scripts/home/light.js
+++ assets/scripts/home/light.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
 	if ( document.querySelector(".light-elem") ) {
+		const mobileLamp = document.querySelector(".header-static__center-light-wrapper");
 		const allLightElements = document.querySelectorAll(".light-elem");
 		setTimeout(() => {
 			document.querySelector(".window-wrapper__window-top-small-bg").style.opacity = "0";
 			for ( let item of allLightElements ) {
 				item.style.opacity = "1";
 			}
+			setTimeout(() => {
+				mobileLamp.classList.remove("mobile-lamp-none");
+			}, 1000)
 		}, 8000)
 	}
 })
